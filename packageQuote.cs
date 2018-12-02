af83f695d5fcc63c272609ce3115b57be21eb813
Rachelle <43661757+mrachelle@users.noreply.github.com>
2018-12-01 21:10:57 -0700

    Package Quote
    an exercise in which the user inputs package info and qualifies the package then calculates a shipping quote

---
+++ packageQuote.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the weight of the package in pounds:");
+            int packageWeight = Console.Read();
+            if (packageWeight >= 50)
+                {
+                    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                    Console.ReadKey();
+                }
+            else
+            {
+                Console.WriteLine("Please enter package width in inches:");
+                int packageWidth = Console.Read();
+                Console.WriteLine("Please enter package length in inches:");
+                int packageLength = Console.Read();
+                int packageDimensions = (packageWidth + packageLength);
+                    if (packageDimensions > 50)
+                    {
+                        Console.WriteLine("Package too big to be shipped via Package Express.");
+                        Console.ReadKey();
+                    }
+                decimal packageQuote = ((packageDimensions * packageWeight) / 100);
+                Console.WriteLine(packageQuote);
+            }
+        Console.ReadKey();
+        }
+    }
+}
